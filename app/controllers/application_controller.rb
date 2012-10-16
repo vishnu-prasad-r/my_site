45474b5cb37ecd64335eb49ed4e68cc85e5cc0ee
Vishnuprasad <vishnu@yubi.in>
2012-10-16 20:10:49 +0530

    Fix5 for courts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
   string= string.downcase
   return true if string== true || string =~ (/(true|t|yes|y|1)$/i)
   return false if string== false || string.nil? || string =~ (/(false|f|no|n|0)$/i)
-  raise ArgumentError.new("invalid value for Boolean: \"#{string}\"")
+  #raise ArgumentError.new("invalid value for Boolean: \"#{string}\"")
+  return false
   end
 end
